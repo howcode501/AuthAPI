b66ad2434761aee6d4caf2c6fac6606daa387dd9
-globla <-global>
2024-10-17 07:25:43 -0700

    The change in User.php 20241017

--- Model/User.php
+++ Model/User.php
@@ -6,4 +6,9 @@ if($state == "login")
 {
     $UserName = $_POST["name"];
     $UserPassword = $_POST["password"];
+
+    if($UserName == "" && $UserPassword == "")
+    {
+        return json_encode(["state" => "validate"]);
+    }
 }
